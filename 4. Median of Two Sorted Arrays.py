05cef75469fb484a36575bda166e7b8c4f6758ea
K.Meghana <kmegha9505@gmail.com>
2025-08-23 15:57:07 +0530

    Update 4. Median of Two Sorted Arrays.py

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -38,6 +38,36 @@ class Solution:
 
 
 class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+        int m = nums1.length, n = nums2.length, left = 0, right = m, half = (m + n + 1) / 2;
+
+        while (left <= right) {
+            int i = (left + right) / 2, j = half - i;
+            int l1 = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
+            int r1 = (i == m) ? Integer.MAX_VALUE : nums1[i];
+            int l2 = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
+            int r2 = (j == n) ? Integer.MAX_VALUE : nums2[j];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((m + n) % 2 == 1) return Math.max(l1, l2);
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            } 
+            if (l1 > r2) right = i - 1; 
+            else left = i + 1;
+        }
+        return 0.0; 
+    }
+}
+
+
+
+
+
+
+                                            ##   OR
+class Solution {
     private int m;
     private int n;
     private int[] nums1;
