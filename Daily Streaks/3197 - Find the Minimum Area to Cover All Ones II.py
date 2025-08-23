d59a0f6462b243d80cdccfa0b87e9c048c16d6c0
K.Meghana <kmegha9505@gmail.com>
2025-08-23 16:02:00 +0530

    Create 3197 - Find the Minimum Area to Cover All Ones II.py

---
+++ Daily Streaks/3197 - Find the Minimum Area to Cover All Ones II.py
@@ -0,0 +1,178 @@
+### PYTHON3
+
+class Solution:
+    def minimumSum(self, grid: List[List[int]]) -> int:
+        def f(i1: int, j1: int, i2: int, j2: int) -> int:
+            x1 = y1 = inf
+            x2 = y2 = -inf
+            for i in range(i1, i2 + 1):
+                for j in range(j1, j2 + 1):
+                    if grid[i][j] == 1:
+                        x1 = min(x1, i)
+                        y1 = min(y1, j)
+                        x2 = max(x2, i)
+                        y2 = max(y2, j)
+            return (x2 - x1 + 1) * (y2 - y1 + 1)
+
+        m, n = len(grid), len(grid[0])
+        ans = m * n
+        for i1 in range(m - 1):
+            for i2 in range(i1 + 1, m - 1):
+                ans = min(
+                    ans,
+                    f(0, 0, i1, n - 1)
+                    + f(i1 + 1, 0, i2, n - 1)
+                    + f(i2 + 1, 0, m - 1, n - 1),
+                )
+        for j1 in range(n - 1):
+            for j2 in range(j1 + 1, n - 1):
+                ans = min(
+                    ans,
+                    f(0, 0, m - 1, j1)
+                    + f(0, j1 + 1, m - 1, j2)
+                    + f(0, j2 + 1, m - 1, n - 1),
+                )
+        for i in range(m - 1):
+            for j in range(n - 1):
+                ans = min(
+                    ans,
+                    f(0, 0, i, j) + f(0, j + 1, i, n - 1) + f(i + 1, 0, m - 1, n - 1),
+                )
+                ans = min(
+                    ans,
+                    f(0, 0, i, n - 1)
+                    + f(i + 1, 0, m - 1, j)
+                    + f(i + 1, j + 1, m - 1, n - 1),
+                )
+
+                ans = min(
+                    ans,
+                    f(0, 0, i, j) + f(i + 1, 0, m - 1, j) + f(0, j + 1, m - 1, n - 1),
+                )
+                ans = min(
+                    ans,
+                    f(0, 0, m - 1, j)
+                    + f(0, j + 1, i, n - 1)
+                    + f(i + 1, j + 1, m - 1, n - 1),
+                )
+        return ans
+
+
+
+
+###   JAVA
+
+
+
+class Solution {
+    private final int inf = 1 << 30;
+    private int[][] grid;
+
+    public int minimumSum(int[][] grid) {
+        this.grid = grid;
+        int m = grid.length;
+        int n = grid[0].length;
+        int ans = m * n;
+
+        for (int i1 = 0; i1 < m - 1; i1++) {
+            for (int i2 = i1 + 1; i2 < m - 1; i2++) {
+                ans = Math.min(
+                    ans, f(0, 0, i1, n - 1) + f(i1 + 1, 0, i2, n - 1) + f(i2 + 1, 0, m - 1, n - 1));
+            }
+        }
+
+        for (int j1 = 0; j1 < n - 1; j1++) {
+            for (int j2 = j1 + 1; j2 < n - 1; j2++) {
+                ans = Math.min(
+                    ans, f(0, 0, m - 1, j1) + f(0, j1 + 1, m - 1, j2) + f(0, j2 + 1, m - 1, n - 1));
+            }
+        }
+
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                ans = Math.min(
+                    ans, f(0, 0, i, j) + f(0, j + 1, i, n - 1) + f(i + 1, 0, m - 1, n - 1));
+                ans = Math.min(
+                    ans, f(0, 0, i, n - 1) + f(i + 1, 0, m - 1, j) + f(i + 1, j + 1, m - 1, n - 1));
+
+                ans = Math.min(
+                    ans, f(0, 0, i, j) + f(i + 1, 0, m - 1, j) + f(0, j + 1, m - 1, n - 1));
+                ans = Math.min(
+                    ans, f(0, 0, m - 1, j) + f(0, j + 1, i, n - 1) + f(i + 1, j + 1, m - 1, n - 1));
+            }
+        }
+        return ans;
+    }
+
+    private int f(int i1, int j1, int i2, int j2) {
+        int x1 = inf, y1 = inf;
+        int x2 = -inf, y2 = -inf;
+        for (int i = i1; i <= i2; i++) {
+            for (int j = j1; j <= j2; j++) {
+                if (grid[i][j] == 1) {
+                    x1 = Math.min(x1, i);
+                    y1 = Math.min(y1, j);
+                    x2 = Math.max(x2, i);
+                    y2 = Math.max(y2, j);
+                }
+            }
+        }
+        return (x2 - x1 + 1) * (y2 - y1 + 1);
+    }
+}
+
+
+
+
+###    C++
+
+
+
+
+class Solution {
+public:
+    int minimumSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = m * n;
+        int inf = INT_MAX / 4;
+        auto f = [&](int i1, int j1, int i2, int j2) {
+            int x1 = inf, y1 = inf;
+            int x2 = -inf, y2 = -inf;
+            for (int i = i1; i <= i2; i++) {
+                for (int j = j1; j <= j2; j++) {
+                    if (grid[i][j] == 1) {
+                        x1 = min(x1, i);
+                        y1 = min(y1, j);
+                        x2 = max(x2, i);
+                        y2 = max(y2, j);
+                    }
+                }
+            }
+            return x1 > x2 || y1 > y2 ? inf : (x2 - x1 + 1) * (y2 - y1 + 1);
+        };
+
+        for (int i1 = 0; i1 < m - 1; i1++) {
+            for (int i2 = i1 + 1; i2 < m - 1; i2++) {
+                ans = min(ans, f(0, 0, i1, n - 1) + f(i1 + 1, 0, i2, n - 1) + f(i2 + 1, 0, m - 1, n - 1));
+            }
+        }
+
+        for (int j1 = 0; j1 < n - 1; j1++) {
+            for (int j2 = j1 + 1; j2 < n - 1; j2++) {
+                ans = min(ans, f(0, 0, m - 1, j1) + f(0, j1 + 1, m - 1, j2) + f(0, j2 + 1, m - 1, n - 1));
+            }
+        }
+
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                ans = min(ans, f(0, 0, i, j) + f(0, j + 1, i, n - 1) + f(i + 1, 0, m - 1, n - 1));
+                ans = min(ans, f(0, 0, i, n - 1) + f(i + 1, 0, m - 1, j) + f(i + 1, j + 1, m - 1, n - 1));
+                ans = min(ans, f(0, 0, i, j) + f(i + 1, 0, m - 1, j) + f(0, j + 1, m - 1, n - 1));
+                ans = min(ans, f(0, 0, m - 1, j) + f(0, j + 1, i, n - 1) + f(i + 1, j + 1, m - 1, n - 1));
+            }
+        }
+
+        return ans;
+    }
+};
