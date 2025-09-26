a542a3cb2932bea0dab7e75dfa5e2ab4452fbe20
K.Meghana <kmegha9505@gmail.com>
2025-09-26 20:35:35 +0530

    Create 611 - Valid Triangle Number.py

---
+++ Daily Streaks/611 - Valid Triangle Number.py
@@ -0,0 +1,57 @@
+####     PYTHON3
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        ans, n = 0, len(nums)
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                k = bisect_left(nums, nums[i] + nums[j], lo=j + 1) - 1
+                ans += k - j
+        return ans
+
+
+
+###   JAVA
+
+
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int res = 0;
+        for (int i = n - 1; i >= 2; --i) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    res += r - l;
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+
+####   C++
+
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                int k = lower_bound(nums.begin() + j + 1, nums.end(), nums[i] + nums[j]) - nums.begin() - 1;
+                ans += k - j;
+            }
+        }
+        return ans;
+    }
+};
